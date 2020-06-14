ee015eb660738943fd588f1cf76778731769e243
Vyacheslav Morozov <31694577+blurryfacebeat@users.noreply.github.com>
2020-06-14 15:39:57 +0700

    Update 2.js

--- Chapter 19(18)/2.js
+++ Chapter 19(18)/2.js
@@ -1,1 +1,10 @@
-
+document.querySelector('#button').addEventListener('click', () => {
+    let code = document.querySelector('#code').value;
+    let outputNode = document.querySelector('#output');
+    try {
+        let result = Function(code)();
+        outputNode.innerText = String(result);
+    } catch (error) {
+        outputNode.innerText = `Error ${error}`;
+    }
+});
