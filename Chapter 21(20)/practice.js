ac6c34e8a59201a6d3b47082b392eef2bd5969d9
Vyacheslav Morozov <31694577+blurryfacebeat@users.noreply.github.com>
2020-06-16 19:38:25 +0700

    Update practice.js

--- Chapter 21(20)/practice.js
+++ Chapter 21(20)/practice.js
@@ -1,1 +1,51 @@
+// 1
+// const { statSync, readdirSync, readFileSync } = require('fs');
 
+const { METHODS } = require('http');
+const { stat } = require('fs');
+
+// let searchTerm = new RegExp(process.argv[2]);
+
+// for (let arg of process.argv.slice(3)) {
+//     search(arg);
+// }
+
+// function search(file) {
+//     let stats = statSync(file);
+//     if (stats.isDirectory()) {
+//         for (let f of readdirync(file)) {
+//             search(file + '/' + f);
+//         }
+//     } else if (searchTerm.test(readFileSync(file, 'utf8'))) {
+//         console.log(file);
+//     }
+// }
+
+// 2
+// const { mkdir } = require('fs').promises;
+
+// methods.MKCOL = async function(req) {
+//     let path = urlPath(req.url);
+//     let stats;
+//     try {
+//         stats = await stat(path);
+//     } catch (error) {
+//         if (error.code != 'ENOENT') {
+//             throw error;
+//         }
+//         await mkdir(path);
+//         return {
+//             status: 204
+//         };
+//     }
+//     if (stats.isDerectory()) {
+//         return {
+//             status: 204
+//         };
+//     } else {
+//         return {
+//             status: 400,
+//             body: 'Not a directory'
+//         };
+//     }
+// }
